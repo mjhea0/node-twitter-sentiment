91a8981710625f790f1d2c7f1c82aa4800231f4c
Michael Herman <hermanmu@gmail.com>
2014-02-18 11:24:24 -0800

    updates

--- twit-decision/app.js
+++ twit-decision/app.js
@@ -8,7 +8,7 @@ var express = require('express'),
   sentimental = require('Sentimental');
 
 // twitter config file
-// var config = require('./config');
+var config = require('./config');
 
 // create express app  
 var app = express();
@@ -33,6 +33,77 @@ if ('development' == app.get('env')) {
 // routes
 app.get('/', routes.index);
 app.get('/ping', routes.ping);
+
+function performAnalysis(tweetSet) {
+	var results = 0;
+	for(var i = 0; i < tweetSet.length; i++) {
+		tweet = tweetSet[i]['text'];
+		retweets = tweetSet[i]['retweet_count'];
+		favorites = tweetSet[i]['favorite_count'];
+		tweet = tweet.replace('#', '');
+		var score = sentimental.analyze(tweet)['score'];
+		results += score;
+		if(score > 0){
+			if(retweets > 0) {
+				results += (Math.log(retweets)/Math.log(2));
+			}
+			if(favorites > 0) {
+				results += (Math.log(favorites)/Math.log(2));
+			}
+		}
+		else if(score < 0){
+			if(retweets > 0) {
+				results -= (Math.log(retweets)/Math.log(2));
+			}
+			if(favorites > 0) {
+				results -= (Math.log(favorites)/Math.log(2));
+			}
+		}
+		else {
+			results += 0;
+		}
+	}
+	results = results / tweetSet.length;
+	return results
+}
+
+app.post('/query', function(req, res) {
+	
+	var choices = JSON.parse(req.body.choices);
+	var today = new Date();
+	
+	var twitter = new twit({
+		consumer_key: config.consumer_key,
+		consumer_secret: config.consumer_secret,
+		access_token: config.access_token,
+		access_token_secret: config.access_token_secret
+	});
+	
+	var highestScore = -Infinity;
+	var highestChoice = null;
+	var array = [];
+	var score = 0;
+	console.log("----------")
+	for(var i = 0; i < choices.length; i++) {
+		(function(i) {
+			array.push(choices[i])
+			twitter.get('search/tweets', {q: '' + choices[i] + ' since:' + today.getFullYear() + '-' + 
+				(today.getMonth() + 1) + '-' + today.getDate(), count:20}, function(err, data) {
+				score = performAnalysis(data['statuses']);
+				console.log("score:", score)
+				console.log("choice:", choices[i])
+				if(score > highestScore) {
+					highestScore = score;
+					highestChoice = choices[i];
+					console.log("winner:",choices[i])
+				}
+				console.log("")
+			});
+		})(i)
+	}
+	setTimeout(function() { res.end(JSON.stringify({'score': highestScore, 'choice': highestChoice})) }, 10000);
+	
+});
 
 // create server
 http.createServer(app).listen(app.get('port'), function(){
---
+++ twit-decision/config_example.js
@@ -0,0 +1,8 @@
+var twitterKeys = {
+  consumer_key: 'your_key',
+  consumer_secret: 'your_key',
+  access_token: 'your_key',
+  access_token_secret: 'your_key'
+}
+
+module.exports = twitterKeys
