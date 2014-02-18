632d827e80864bb4e161161efd55d302172e1eb8
Michael Herman <hermanmu@gmail.com>
2014-02-18 11:28:38 -0800

    updates

--- twitter-decider/config_example.js
+++
@@ -1,8 +0,0 @@
-var twitterKeys = {
-	consumer_key: 'your_key',
-	consumer_secret: 'your_key',
-	access_token: 'your_key',
-	access_token_secret: 'your_key'
-}
-
-module.exports = twitterKeys
--- twitter-decider/routes/index.js
+++
@@ -1,9 +0,0 @@
-var path = require("path");
-
-exports.index = function(req, res){
-  res.render('index', { title: "Twitter Decider"});
-};
-
-exports.ping = function(req, res){
-  res.send("pong!", 200);
-};
